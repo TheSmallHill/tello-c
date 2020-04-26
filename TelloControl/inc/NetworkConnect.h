56aba6c87e80397213b150d373a71ee17fb7df19
TheSmallHill <jacob.knoll16@gmail.com>
2020-04-26 13:55:20 -0500

    Filling in the dbus functionality

--- TelloControl/inc/NetworkConnect.h
+++ TelloControl/inc/NetworkConnect.h
@@ -1,6 +1,17 @@
 #include <dbus/dbus.h>
 
+#include <string>
+
 class NetworkConnect
 {
+public:
+   NetworkConnect(const std::string& ssid, const std::string& password);
+   
+   ~NetworkConnect();
 
+protected:
+   DBusError error_;
+   DBusConnection* connection_;
+   DBusMessage* message_;
+   DBusMessage* reply_;
 }
--- TelloControl/src/NetworkConnect.cpp
+++ TelloControl/src/NetworkConnect.cpp
@@ -1,1 +1,3 @@
 #include "NetworkConnect.h"
+
+
